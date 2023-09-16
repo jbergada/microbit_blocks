4bd76f212fbf2e3bf0c0a8d47b488a670c006c00
jbergada <jbergada@xtec.cat>
2023-09-16 19:49:54 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,16 @@
+let list: number[] = []
 let value = 0
-let list2 : number[] = []
 basic.forever(function () {
-    list2 = [
+    list = [
     2,
     4,
     6,
     8
     ]
-    for (let value2 of list2) {
-        serial.writeValue("list", _py.py_array_index(list2, value2))
-        serial.writeValue("value", value2)
-        basic.showNumber(value2)
+    for (let value of list) {
+        serial.writeValue("list", list.indexOf(value))
+        serial.writeValue("value", value)
+        basic.showNumber(value)
         pins.digitalWritePin(DigitalPin.P0, 0)
         basic.pause(500)
         pins.digitalWritePin(DigitalPin.P0, 1)
